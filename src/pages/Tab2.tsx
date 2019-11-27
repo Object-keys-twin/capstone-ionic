5092bb18bb13fde90f3a29835d0da89459d0cbec
Erin Fung <erinfungg@gmail.com>
2019-11-26 20:35:00 -0500

    got checkpoints for each tour to show

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -9,34 +9,38 @@ import {
 	IonInput,
 	IonList,
 	IonItem,
-	IonLabel
+	IonLabel,
+	IonRow,
+	IonCol,
+	IonGrid
 } from "@ionic/react";
 import React, { Component } from "react";
 import { Plugins } from "@capacitor/core";
 import db from "../firebase/firebase";
 import "./Tab2.css";
 const { Geolocation } = Plugins;
-
 interface DbData {
-	checkpoints: Array<string>;
+	checkpoints: Array<any>;
 	description: string;
 	name: string;
 	timestamp: object;
 	upvotes: number;
 	user: string;
 }
-
+// interface CPData {
+// 	lat: number;
+// 	long: number;
+// 	name: string;
+// 	rating: number;
+// }
 type State = {
 	tours: Array<DbData>;
 };
-
 class PublicTours extends Component<{}, State> {
 	state = { tours: Array<DbData>() };
-
 	componentDidMount() {
 		this.getTours();
 	}
-
 	getTours = () => {
 		db.collection("tours")
 			.get()
@@ -57,10 +61,27 @@ class PublicTours extends Component<{}, State> {
 					});
 					//console.log(this.state.tours);
 				});
+				this.state.tours.forEach((tour, id) => {
+					this.getCheckpoints(tour, id);
+				});
 			});
 		// .then(docs => (this.setState({tours: docs})))
 	};
-
+	getCheckpoints = async (tour: any, idx: number) => {
+		let checkpointsWithData: any = [];
+		for (let i = 0; i < tour.checkpoints.length; i++) {
+			const checkpoints = await db
+				.collection("checkpoints")
+				.doc(`${tour.checkpoints[i]}`)
+				.get();
+			checkpointsWithData.push(checkpoints.data());
+		}
+		let tours = this.state.tours;
+		tours.map((el, i) => {
+			if (i === idx) el.checkpoints = checkpointsWithData;
+		});
+		this.setState({ tours });
+	};
 	// async getCurrentPosition() {
 	// 	const coordinates = await Geolocation.getCurrentPosition();
 	// 	this.setState({
@@ -68,11 +89,9 @@ class PublicTours extends Component<{}, State> {
 	// 		longitude: coordinates.coords.longitude
 	// 	});
 	// }
-
 	// updateText() {
 	// 	this.setState({ text });
 	// }
-
 	render() {
 		const { tours } = this.state;
 		console.log(tours);
@@ -94,12 +113,17 @@ class PublicTours extends Component<{}, State> {
 					<IonList>
 						{tours.map((tour, idx) => (
 							<IonItem key={idx} onClick={() => {}} className="mainListRow">
-								<IonLabel>{tour.name}</IonLabel>
-								<IonList>
-									{tour.checkpoints.map(checkpoint => (
-										<IonItem>{checkpoint}</IonItem>
-									))}
-								</IonList>
+								<IonGrid>
+									<IonCol>
+										<IonLabel>{tour.name}</IonLabel>
+										<IonList>
+											{tour.checkpoints.map(checkpoint => {
+												if (checkpoint)
+													return <IonItem>{checkpoint.name}</IonItem>;
+											})}
+										</IonList>
+									</IonCol>
+								</IonGrid>
 							</IonItem>
 						))}
 					</IonList>
@@ -108,5 +132,4 @@ class PublicTours extends Component<{}, State> {
 		);
 	}
 }
-
 export default PublicTours;
