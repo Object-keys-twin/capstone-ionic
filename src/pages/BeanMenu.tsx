0c841c637d1aa7c2bf19540772efad928e9ff8ce
Ashton Schneider <asch8216@gmail.com>
2019-12-05 17:27:42 -0500

    functional delete from stringbean

--- src/pages/BeanMenu.tsx
+++ src/pages/BeanMenu.tsx
@@ -1,148 +1,169 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
-  IonMenu,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonList,
-  IonItem,
-  IonRouterOutlet,
-  IonButtons,
-  IonButton,
-  IonMenuButton
-} from "@ionic/react";
+	IonMenu,
+	IonHeader,
+	IonToolbar,
+	IonTitle,
+	IonContent,
+	IonList,
+	IonItem,
+	IonRouterOutlet,
+	IonButtons,
+	IonButton,
+	IonMenuButton,
+	IonItemSliding,
+	IonItemOptions,
+	IonItemOption,
+	IonLabel,
+	IonIcon
+} from '@ionic/react'
 
-import { menuController } from "@ionic/core";
-import { Plugins } from "@capacitor/core";
-import db from "../firebase/firebase";
-import BeanMenuForm from "./BeanMenuForm";
-import firebase from "firebase";
-const { Storage } = Plugins;
+import { menuController } from '@ionic/core'
+import { Plugins } from '@capacitor/core'
+import db from '../firebase/firebase'
+import BeanMenuForm from './BeanMenuForm'
+import { trash } from 'ionicons/icons'
+import firebase from 'firebase'
+const { Storage } = Plugins
 
 interface BusinessData {
-  id: string;
-  name: string;
-  location: object;
-  imageUrl: string;
-  categories: Array<object>;
-  rating?: number;
-  latitude: number;
-  longitude: number;
-  price?: string | undefined;
+	id: string
+	name: string
+	location: object
+	imageUrl: string
+	categories: Array<object>
+	rating?: number
+	latitude: number
+	longitude: number
+	price?: string | undefined
 }
 
 type State = {
-  stringbean: Array<BusinessData>;
-  showAlert: boolean;
-  name: string;
-  description: string;
-};
+	stringbean: Array<BusinessData>
+	showAlert: boolean
+	name: string
+	description: string
+}
 
 type Props = {
-  stringbean: Array<BusinessData>;
-};
+	stringbean: Array<BusinessData>
+	removeFromStringBean: (id: string) => void
+}
 
 export default class BeanMenu extends Component<Props, State> {
-  state = {
-    stringbean: Array<BusinessData>(),
-    showAlert: false,
-    name: "",
-    description: ""
-  };
+	state = {
+		stringbean: Array<BusinessData>(),
+		showAlert: false,
+		name: '',
+		description: ''
+	}
 
-  toggleAlert = () => {
-    this.setState({ showAlert: !this.state.showAlert });
-  };
+	toggleAlert = () => {
+		this.setState({ showAlert: !this.state.showAlert })
+	}
 
-  createOrUpdateCheckpoint = async (bean: BusinessData) => {
-    let beanRef = db.collection("checkpoints").doc(bean.id);
-    let getBean = await beanRef.get();
-    if (!getBean.data()) {
-      if (bean.price === undefined) {
-        bean.price = "not available";
-      }
-      await beanRef.set(bean);
-    }
-  };
+	createOrUpdateCheckpoint = async (bean: BusinessData) => {
+		let beanRef = db.collection('checkpoints').doc(bean.id)
+		let getBean = await beanRef.get()
+		if (!getBean.data()) {
+			if (bean.price === undefined) {
+				bean.price = 'not available'
+			}
+			await beanRef.set(bean)
+		}
+	}
 
-  publishTour = async (name: string, description: string) => {
-    let checkpoints = Array<string>();
+	publishTour = async (name: string, description: string) => {
+		let checkpoints = Array<string>()
 
-    this.props.stringbean.forEach(async bean => {
-      await this.createOrUpdateCheckpoint(bean);
-    });
+		this.props.stringbean.forEach(async bean => {
+			await this.createOrUpdateCheckpoint(bean)
+		})
 
-    this.props.stringbean.forEach(bean => {
-      checkpoints.push(bean.id);
-    });
+		this.props.stringbean.forEach(bean => {
+			checkpoints.push(bean.id)
+		})
 
-    let userEmail: string = "";
-    const data = await Storage.get({ key: "user" });
-    if (data.value) {
-      userEmail = JSON.parse(data.value).email;
-    }
+		let userEmail: string = ''
+		const data = await Storage.get({ key: 'user' })
+		if (data.value) {
+			userEmail = JSON.parse(data.value).email
+		}
 
-    let tour = {
-      checkpoints,
-      name: name,
-      description: description,
-      created: firebase.firestore.Timestamp.fromDate(new Date()),
-      user: userEmail
-    };
+		let tour = {
+			checkpoints,
+			name: name,
+			description: description,
+			created: firebase.firestore.Timestamp.fromDate(new Date()),
+			user: userEmail
+		}
 
-    await db
-      .collection("tours")
-      .add(tour)
-      .then(ref => {
-        console.log("Added document with ID: ", ref.id);
-      });
+		await db
+			.collection('tours')
+			.add(tour)
+			.then(ref => {
+				console.log('Added document with ID: ', ref.id)
+			})
 
-    await Storage.remove({ key: "stringbean" });
-  };
+		await Storage.remove({ key: 'stringbean' })
+	}
 
-  render() {
-    return (
-      <>
-        <IonMenu side="end" contentId="main" type="overlay" swipeGesture={true}>
-          <IonHeader>
-            <IonToolbar color="primary">
-              <IonTitle>My Beans</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent>
-            <IonList>
-              {this.props.stringbean.map((bean, idx) => (
-                <IonItem key={idx}>{bean.name}</IonItem>
-              ))}
-            </IonList>
-          </IonContent>
-          <IonButton
-            onClick={() => {
-              this.toggleAlert();
-            }}
-          >
-            Publish Stringbean
-          </IonButton>
-          <BeanMenuForm
-            showAlert={this.state.showAlert}
-            toggleAlert={this.toggleAlert}
-            publishTour={this.publishTour}
-            // handleChange={this.handleChange}
-          />
-        </IonMenu>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="end">
-              <IonMenuButton
-                autoHide={false}
-                onClick={() => menuController.open}
-              ></IonMenuButton>
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonRouterOutlet id="main"></IonRouterOutlet>
-      </>
-    );
-  }
+	render() {
+		return (
+			<>
+				<IonMenu side="end" contentId="main" type="overlay" swipeGesture={true}>
+					<IonHeader>
+						<IonToolbar color="primary">
+							<IonTitle>My Beans</IonTitle>
+						</IonToolbar>
+					</IonHeader>
+					<IonContent>
+						<IonList>
+							{this.props.stringbean.map((bean, idx) => (
+								<IonItemSliding key={idx}>
+									<IonItemOptions side="end">
+										<IonItemOption
+											color="danger"
+											onClick={() => {
+												this.props.removeFromStringBean(bean.id)
+											}}
+										>
+											<IonIcon slot="icon-only" icon={trash}></IonIcon>
+										</IonItemOption>
+									</IonItemOptions>
+									<IonItem>
+										<IonItem>{bean.name}</IonItem>
+									</IonItem>
+								</IonItemSliding>
+							))}
+						</IonList>
+					</IonContent>
+					<IonButton
+						onClick={() => {
+							this.toggleAlert()
+						}}
+					>
+						Publish Stringbean
+					</IonButton>
+					<BeanMenuForm
+						showAlert={this.state.showAlert}
+						toggleAlert={this.toggleAlert}
+						publishTour={this.publishTour}
+						// handleChange={this.handleChange}
+					/>
+				</IonMenu>
+				<IonHeader>
+					<IonToolbar>
+						<IonButtons slot="end">
+							<IonMenuButton
+								autoHide={false}
+								onClick={() => menuController.open}
+							></IonMenuButton>
+						</IonButtons>
+					</IonToolbar>
+				</IonHeader>
+				<IonRouterOutlet id="main"></IonRouterOutlet>
+			</>
+		)
+	}
 }
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,211 +1,237 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
-  IonHeader,
-  IonToolbar,
-  IonPage,
-  IonTitle,
-  IonContent,
-  IonSearchbar,
-  IonItem,
-  IonButton,
-  IonModal,
-  IonSpinner,
-  IonLabel
-} from "@ionic/react";
-import { Plugins } from "@capacitor/core";
-import axios from "axios";
-import BeanMenu from "./BeanMenu";
-import { yelpApiKey } from "../secrets";
+	IonHeader,
+	IonToolbar,
+	IonPage,
+	IonTitle,
+	IonContent,
+	IonSearchbar,
+	IonItem,
+	IonButton,
+	IonModal,
+	IonSpinner,
+	IonLabel
+} from '@ionic/react'
+import { Plugins } from '@capacitor/core'
+import axios from 'axios'
+import BeanMenu from './BeanMenu'
+import { yelpApiKey } from '../secrets'
+import { tsConstructSignatureDeclaration } from '@babel/types'
 
-const { Geolocation, Storage } = Plugins;
+const { Geolocation, Storage } = Plugins
 
 interface BusinessData {
-  id: string;
-  name: string;
-  location: object;
-  imageUrl: string;
-  categories: Array<object>;
-  rating?: number;
-  latitude: number;
-  longitude: number;
-  price?: string | undefined;
+	id: string
+	name: string
+	location: object
+	imageUrl: string
+	categories: Array<object>
+	rating?: number
+	latitude: number
+	longitude: number
+	price?: string | undefined
 }
 
 type State = {
-  latitude: number;
-  longitude: number;
-  businesses: Array<BusinessData>;
-  search: string;
-  showModal: number;
-  stringbean: Array<BusinessData>;
-};
-
-class CreateStory extends Component<{}, State> {
-  state = {
-    latitude: 0,
-    longitude: 0,
-    businesses: Array<BusinessData>(),
-    search: "",
-    showModal: Infinity,
-    stringbean: Array<BusinessData>()
-  };
-
-  componentDidMount() {
-    this.getCurrentPosition();
-    this.getStringBeanOnMount();
-  }
-
-  getCurrentPosition = async () => {
-    const coordinates = await Geolocation.getCurrentPosition();
-    this.setState(
-      {
-        latitude: coordinates.coords.latitude,
-        longitude: coordinates.coords.longitude
-      },
-      () => {
-        this.getYelp(this.state.latitude, this.state.longitude);
-      }
-    );
-  };
-
-  getStringBeanOnMount = async () => {
-    const data = await Storage.get({ key: "stringbean" });
-    if (data.value) {
-      this.setState({ stringbean: JSON.parse(data.value) });
-    }
-  };
-
-  getYelp = async (latitude: number, longitude: number, term?: string) => {
-    const api = axios.create({
-      baseURL: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3",
-      headers: {
-        Authorization: `Bearer ${yelpApiKey}`
-      }
-    });
-    const { data } = await api.get("/businesses/search", {
-      params: {
-        limit: 20,
-        latitude: latitude,
-        longitude: longitude,
-        term: term
-      }
-    });
-
-    const info = data.businesses.map((business: any) => ({
-      id: business.id,
-      name: business.name,
-      location:
-        business.location.display_address[0] +
-        ", " +
-        business.location.display_address[1],
-      latitude: business.coordinates.latitude,
-      longitude: business.coordinates.longitude,
-      price: business.price,
-      imageUrl: business.image_url,
-      categories: business.categories,
-      rating: business.rating
-    }));
-
-    this.setState({ businesses: info });
-  };
-
-  handleChange = (e: string) => {
-    this.setState({ search: e });
-  };
-
-  addToStringBean = async (business: object) => {
-    let stringBeanArray = [];
-    const localStorage = await Storage.get({ key: "stringbean" });
-    if (localStorage.value) {
-      stringBeanArray = JSON.parse(localStorage.value);
-    }
-    stringBeanArray.push(business);
-    this.setState({ stringbean: stringBeanArray });
-    await Storage.set({
-      key: "stringbean",
-      value: JSON.stringify(stringBeanArray)
-    });
-    // console.log(await Storage.get({ key: "stringbean" }));
-    // console.log(localStorage);
-  };
-
-  //write function to publish story. loops through the checkpoints, checks if they exist in the database. if not, adds it to the database. grabs the checkpoint's firestore ID. holds the checkpoints' firestore IDs in an array.
-  //then build the tour object. use the array of checkpoint firestore IDs.
-
-  render() {
-    // console.log("everything", this.state);
-
-    const { businesses } = this.state;
-    // console.log(Storage.get({ key: "stringbean" }));
-
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Beans</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <BeanMenu stringbean={this.state.stringbean} />
-        <IonSearchbar
-          onIonChange={e =>
-            this.handleChange((e.target as HTMLInputElement).value)
-          }
-        >
-          <IonButton
-            size="small"
-            onClick={() =>
-              this.getYelp(
-                this.state.latitude,
-                this.state.longitude,
-                this.state.search
-              )
-            }
-          >
-            Search
-          </IonButton>
-        </IonSearchbar>
-
-        {businesses.length ? (
-          <IonContent>
-            {businesses.map((business, idx) => (
-              <IonItem key={idx}>
-                <IonItem onClick={() => this.setState({ showModal: idx })}>
-                  {business.name} <br></br>
-                  {business.location}
-                </IonItem>
-
-                <IonModal isOpen={idx === this.state.showModal}>
-                  <h1>{business.name}</h1>
-                  <p>{business.location}</p>
-                  <IonButton
-                    onClick={() => {
-                      this.addToStringBean(business);
-                      this.setState({ showModal: Infinity });
-                    }}
-                  >
-                    Add To Stringbean
-                  </IonButton>
-                  <IonButton
-                    onClick={() => {
-                      this.setState({ showModal: Infinity });
-                    }}
-                  >
-                    Back
-                  </IonButton>
-                </IonModal>
-              </IonItem>
-            ))}
-          </IonContent>
-        ) : (
-          <IonContent>
-            <IonSpinner class="spinner"></IonSpinner>
-          </IonContent>
-        )}
-      </IonPage>
-    );
-  }
+	latitude: number
+	longitude: number
+	businesses: Array<BusinessData>
+	search: string
+	showModal: number
+	stringbean: Array<BusinessData>
 }
 
-export default CreateStory;
+class CreateStory extends Component<{}, State> {
+	state = {
+		latitude: 0,
+		longitude: 0,
+		businesses: Array<BusinessData>(),
+		search: '',
+		showModal: Infinity,
+		stringbean: Array<BusinessData>()
+	}
+
+	componentDidMount() {
+		this.getCurrentPosition()
+		this.getStringBeanOnMount()
+	}
+
+	getCurrentPosition = async () => {
+		const coordinates = await Geolocation.getCurrentPosition()
+		this.setState(
+			{
+				latitude: coordinates.coords.latitude,
+				longitude: coordinates.coords.longitude
+			},
+			() => {
+				this.getYelp(this.state.latitude, this.state.longitude)
+			}
+		)
+	}
+
+	getStringBeanOnMount = async () => {
+		const data = await Storage.get({ key: 'stringbean' })
+		if (data.value) {
+			this.setState({ stringbean: JSON.parse(data.value) })
+		}
+	}
+
+	removeFromStringBean = async (id: string) => {
+		let storage: any
+		let parsedStorage: any
+		storage = await Storage.get({
+			key: 'stringbean'
+		})
+		// console.log(typeof storage.value)
+		parsedStorage = JSON.parse(storage.value)
+		console.log(parsedStorage)
+		console.log(id)
+		const removedBean = parsedStorage.filter(
+			(item: BusinessData) => item.id !== id
+		)
+		this.setState({
+			stringbean: removedBean
+		})
+		await Storage.set({
+			key: 'stringbean',
+			value: JSON.stringify(removedBean)
+		})
+	}
+
+	getYelp = async (latitude: number, longitude: number, term?: string) => {
+		const api = axios.create({
+			baseURL: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3',
+			headers: {
+				Authorization: `Bearer ${yelpApiKey}`
+			}
+		})
+		const { data } = await api.get('/businesses/search', {
+			params: {
+				limit: 20,
+				latitude: latitude,
+				longitude: longitude,
+				term: term
+			}
+		})
+
+		const info = data.businesses.map((business: any) => ({
+			id: business.id,
+			name: business.name,
+			location:
+				business.location.display_address[0] +
+				', ' +
+				business.location.display_address[1],
+			latitude: business.coordinates.latitude,
+			longitude: business.coordinates.longitude,
+			price: business.price,
+			imageUrl: business.image_url,
+			categories: business.categories,
+			rating: business.rating
+		}))
+
+		this.setState({ businesses: info })
+	}
+
+	handleChange = (e: string) => {
+		this.setState({ search: e })
+	}
+
+	addToStringBean = async (business: object) => {
+		let stringBeanArray = []
+		const localStorage = await Storage.get({ key: 'stringbean' })
+		if (localStorage.value) {
+			stringBeanArray = JSON.parse(localStorage.value)
+		}
+		stringBeanArray.push(business)
+		this.setState({ stringbean: stringBeanArray })
+		await Storage.set({
+			key: 'stringbean',
+			value: JSON.stringify(stringBeanArray)
+		})
+		// console.log(await Storage.get({ key: "stringbean" }));
+		// console.log(localStorage);
+	}
+
+	//write function to publish story. loops through the checkpoints, checks if they exist in the database. if not, adds it to the database. grabs the checkpoint's firestore ID. holds the checkpoints' firestore IDs in an array.
+	//then build the tour object. use the array of checkpoint firestore IDs.
+
+	render() {
+		// console.log("everything", this.state);
+
+		const { businesses } = this.state
+		// console.log(Storage.get({ key: "stringbean" }));
+
+		return (
+			<IonPage>
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle>Beans</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<BeanMenu
+					stringbean={this.state.stringbean}
+					removeFromStringBean={this.removeFromStringBean}
+				/>
+				<IonSearchbar
+					onIonChange={e =>
+						this.handleChange((e.target as HTMLInputElement).value)
+					}
+				>
+					<IonButton
+						size="small"
+						onClick={() =>
+							this.getYelp(
+								this.state.latitude,
+								this.state.longitude,
+								this.state.search
+							)
+						}
+					>
+						Search
+					</IonButton>
+				</IonSearchbar>
+
+				{businesses.length ? (
+					<IonContent>
+						{businesses.map((business, idx) => (
+							<IonItem key={idx}>
+								<IonItem onClick={() => this.setState({ showModal: idx })}>
+									{business.name} <br></br>
+									{business.location}
+								</IonItem>
+
+								<IonModal isOpen={idx === this.state.showModal}>
+									<h1>{business.name}</h1>
+									<p>{business.location}</p>
+									<IonButton
+										onClick={() => {
+											this.addToStringBean(business)
+											this.setState({ showModal: Infinity })
+										}}
+									>
+										Add To Stringbean
+									</IonButton>
+									<IonButton
+										onClick={() => {
+											this.setState({ showModal: Infinity })
+										}}
+									>
+										Back
+									</IonButton>
+								</IonModal>
+							</IonItem>
+						))}
+					</IonContent>
+				) : (
+					<IonContent>
+						<IonSpinner class="spinner"></IonSpinner>
+					</IonContent>
+				)}
+			</IonPage>
+		)
+	}
+}
+
+export default CreateStory
 
 // onSubmit?: ((event: React.FormEvent<HTMLIonSearchbarElement>) => void)
