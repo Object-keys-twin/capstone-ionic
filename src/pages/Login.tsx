969ad50b54fb5976512f6f0818b2533ca36ad6e9
Ashton Schneider <asch8216@gmail.com>
2019-12-07 11:43:57 -0500

    added front end error if password too short

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,168 +1,170 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonButton,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonIcon
-} from "@ionic/react";
-import { eye, eyeOff } from "ionicons/icons";
-import "./Login.css";
-import SignUp from "./SignUp";
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonButton,
+	IonItem,
+	IonLabel,
+	IonInput,
+	IonIcon
+} from '@ionic/react'
+import { eye, eyeOff } from 'ionicons/icons'
+import './Login.css'
+import SignUp from './SignUp'
 
 enum PasswordVisibility {
-  Password = "password",
-  Text = "text"
+	Password = 'password',
+	Text = 'text'
 }
 
 interface userData {
-  email: string | null;
-  uid?: string | null;
-  displayName: string | null;
-  photoURL: string | null;
-  password: string;
+	email: string | null
+	uid?: string | null
+	displayName: string | null
+	photoURL: string | null
+	password: string
 }
 
 type Props = {
-  handleGoogle: () => void;
-  handleSubmit: (user: userData, type?: string) => void;
-  logInError: boolean;
-};
+	handleGoogle: () => void
+	handleSubmit: (user: userData, type?: string) => void
+	logInError: boolean
+	signUpError: boolean
+}
 
 type State = {
-  passwordVisibility: boolean;
-  displayName: string;
-  email: string;
-  password: string;
-  showSignUp: boolean;
-};
+	passwordVisibility: boolean
+	displayName: string
+	email: string
+	password: string
+	showSignUp: boolean
+}
 
 class Login extends Component<Props, State> {
-  state = {
-    passwordVisibility: false,
-    displayName: "",
-    email: "",
-    password: "",
-    showSignUp: false
-  };
+	state = {
+		passwordVisibility: false,
+		displayName: '',
+		email: '',
+		password: '',
+		showSignUp: false
+	}
 
-  togglePassword = () => {
-    this.setState({
-      passwordVisibility: !this.state.passwordVisibility
-    });
-  };
+	togglePassword = () => {
+		this.setState({
+			passwordVisibility: !this.state.passwordVisibility
+		})
+	}
 
-  handleEmail = (e: string) => {
-    this.setState({ email: e });
-  };
+	handleEmail = (e: string) => {
+		this.setState({ email: e })
+	}
 
-  handlePassword = (e: string) => {
-    this.setState({ password: e });
-  };
+	handlePassword = (e: string) => {
+		this.setState({ password: e })
+	}
 
-  showSignUp = () => {
-    this.setState({ showSignUp: !this.state.showSignUp });
-  };
+	showSignUp = () => {
+		this.setState({ showSignUp: !this.state.showSignUp })
+	}
 
-  logIn = () => {
-    let tryLogIn = {
-      email: this.state.email,
-      uid: "",
-      displayName: "",
-      photoURL: "",
-      password: this.state.password
-    };
-    this.props.handleSubmit(tryLogIn);
-  };
+	logIn = () => {
+		let tryLogIn = {
+			email: this.state.email,
+			uid: '',
+			displayName: '',
+			photoURL: '',
+			password: this.state.password
+		}
+		this.props.handleSubmit(tryLogIn)
+	}
 
-  render() {
-    if (!this.state.showSignUp) {
-      return (
-        <IonPage>
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle className="header">Login</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent>
-            <IonItem>
-              <IonLabel>Email</IonLabel>
-              <IonInput
-                required
-                clearInput
-                type="email"
-                placeholder="mrbean123@email.com"
-                onIonChange={e =>
-                  this.handleEmail((e.target as HTMLInputElement).value)
-                }
-              ></IonInput>
-            </IonItem>
-            <IonItem>
-              <IonLabel>Password</IonLabel>
-              <IonInput
-                required
-                clearInput
-                onIonChange={e =>
-                  this.handlePassword((e.target as HTMLInputElement).value)
-                }
-                type={
-                  this.state.passwordVisibility === false
-                    ? PasswordVisibility.Password
-                    : PasswordVisibility.Text
-                }
-                placeholder="123123123"
-              ></IonInput>
+	render() {
+		if (!this.state.showSignUp) {
+			return (
+				<IonPage>
+					<IonHeader>
+						<IonToolbar>
+							<IonTitle className="header">Login</IonTitle>
+						</IonToolbar>
+					</IonHeader>
+					<IonContent>
+						<IonItem>
+							<IonLabel>Email</IonLabel>
+							<IonInput
+								required
+								clearInput
+								type="email"
+								placeholder="mrbean123@email.com"
+								onIonChange={e =>
+									this.handleEmail((e.target as HTMLInputElement).value)
+								}
+							></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel>Password</IonLabel>
+							<IonInput
+								required
+								clearInput
+								onIonChange={e =>
+									this.handlePassword((e.target as HTMLInputElement).value)
+								}
+								type={
+									this.state.passwordVisibility === false
+										? PasswordVisibility.Password
+										: PasswordVisibility.Text
+								}
+								placeholder="123123123"
+							></IonInput>
 
-              {this.state.password ? (
-                <IonIcon
-                  icon={this.state.passwordVisibility ? eyeOff : eye}
-                  onClick={this.togglePassword}
-                />
-              ) : (
-                <IonItem />
-              )}
-            </IonItem>
-            <IonButton
-              onClick={() => {
-                this.logIn();
-              }}
-            >
-              Log In
-            </IonButton>
-            <IonButton
-              onClick={() => {
-                this.props.handleGoogle();
-              }}
-            >
-              Login with Google
-            </IonButton>
-            <IonButton
-              onClick={() => {
-                this.showSignUp();
-              }}
-            >
-              Sign Up
-            </IonButton>
-            {this.props.logInError ? (
-              <IonItem>Wrong email/password!</IonItem>
-            ) : (
-              <IonItem></IonItem>
-            )}
-          </IonContent>
-        </IonPage>
-      );
-    }
-    return (
-      <SignUp
-        handleSubmit={this.props.handleSubmit}
-        showSignUp={this.showSignUp}
-      />
-    );
-  }
+							{this.state.password ? (
+								<IonIcon
+									icon={this.state.passwordVisibility ? eyeOff : eye}
+									onClick={this.togglePassword}
+								/>
+							) : (
+								<IonItem />
+							)}
+						</IonItem>
+						<IonButton
+							onClick={() => {
+								this.logIn()
+							}}
+						>
+							Log In
+						</IonButton>
+						<IonButton
+							onClick={() => {
+								this.props.handleGoogle()
+							}}
+						>
+							Login with Google
+						</IonButton>
+						<IonButton
+							onClick={() => {
+								this.showSignUp()
+							}}
+						>
+							Sign Up
+						</IonButton>
+						{this.props.logInError ? (
+							<IonItem>Wrong email/password!</IonItem>
+						) : (
+							<IonItem></IonItem>
+						)}
+					</IonContent>
+				</IonPage>
+			)
+		}
+		return (
+			<SignUp
+				handleSubmit={this.props.handleSubmit}
+				showSignUp={this.showSignUp}
+				signUpError={this.props.signUpError}
+			/>
+		)
+	}
 }
-export default Login;
+export default Login
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,147 +1,151 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonButton,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonIcon
-} from "@ionic/react";
-import { eye, eyeOff } from "ionicons/icons";
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonButton,
+	IonItem,
+	IonLabel,
+	IonInput,
+	IonIcon
+} from '@ionic/react'
+import { eye, eyeOff } from 'ionicons/icons'
 
 enum PasswordVisibility {
-  Password = "password",
-  Text = "text"
+	Password = 'password',
+	Text = 'text'
 }
 
 interface userData {
-  email: string | null;
-  uid?: string | null;
-  displayName: string | null;
-  photoURL: string | null;
-  password: string;
+	email: string | null
+	uid?: string | null
+	displayName: string | null
+	photoURL: string | null
+	password: string
 }
 
 type Props = {
-  handleSubmit: (user: userData, type?: string) => void;
-  showSignUp: () => void;
-};
+	handleSubmit: (user: userData, type?: string) => void
+	showSignUp: () => void
+	signUpError: boolean
+}
 
 type State = {
-  passwordVisibility: boolean;
-  displayName: string;
-  email: string;
-  password: string;
-  showSignUp: boolean;
-};
+	passwordVisibility: boolean
+	displayName: string
+	email: string
+	password: string
+	showSignUp: boolean
+}
 
 export default class SignUp extends Component<Props, State> {
-  state = {
-    passwordVisibility: false,
-    displayName: "",
-    email: "",
-    password: "",
-    showSignUp: false
-  };
+	state = {
+		passwordVisibility: false,
+		displayName: '',
+		email: '',
+		password: '',
+		showSignUp: false
+	}
 
-  handleEmail = (e: string) => {
-    this.setState({ email: e });
-  };
+	handleEmail = (e: string) => {
+		this.setState({ email: e })
+	}
 
-  handlePassword = (e: string) => {
-    this.setState({ password: e });
-  };
+	handlePassword = (e: string) => {
+		this.setState({ password: e })
+	}
 
-  handleDisplayName = (e: string) => {
-    this.setState({ displayName: e });
-  };
+	handleDisplayName = (e: string) => {
+		this.setState({ displayName: e })
+	}
 
-  togglePassword = () => {
-    this.setState({
-      passwordVisibility: !this.state.passwordVisibility
-    });
-  };
+	togglePassword = () => {
+		this.setState({
+			passwordVisibility: !this.state.passwordVisibility
+		})
+	}
 
-  createAccount = () => {
-    let newUser = {
-      email: this.state.email,
-      uid: "",
-      displayName: this.state.displayName,
-      photoURL: "",
-      password: this.state.password
-    };
-    this.props.handleSubmit(newUser, "signup");
-  };
+	createAccount = () => {
+		let newUser = {
+			email: this.state.email,
+			uid: '',
+			displayName: this.state.displayName,
+			photoURL: '',
+			password: this.state.password
+		}
+		this.props.handleSubmit(newUser, 'signup')
+	}
 
-  render() {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle className="header">Sign Up</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          <IonItem>
-            <IonLabel>Username</IonLabel>
-            <IonInput
-              required
-              clearInput
-              class="input"
-              name="displayName"
-              placeholder="mrbean123"
-              onIonChange={e =>
-                this.handleDisplayName((e.target as HTMLInputElement).value)
-              }
-            ></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Email</IonLabel>
-            <IonInput
-              required
-              clearInput
-              type="email"
-              placeholder="mrbean123@email.com"
-              name="email"
-              onIonChange={e =>
-                this.handleEmail((e.target as HTMLInputElement).value)
-              }
-            ></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Password</IonLabel>
-            <IonInput
-              required
-              clearInput
-              onIonChange={e =>
-                this.handlePassword((e.target as HTMLInputElement).value)
-              }
-              type={
-                this.state.passwordVisibility === false
-                  ? PasswordVisibility.Password
-                  : PasswordVisibility.Text
-              }
-              placeholder="123123123"
-              name="password"
-            ></IonInput>
+	render() {
+		return (
+			<IonPage>
+				<IonHeader>
+					<IonToolbar>
+						<IonTitle className="header">Sign Up</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonContent>
+					<IonItem>
+						<IonLabel>Username</IonLabel>
+						<IonInput
+							required
+							clearInput
+							class="input"
+							name="displayName"
+							placeholder="mrbean123"
+							onIonChange={e =>
+								this.handleDisplayName((e.target as HTMLInputElement).value)
+							}
+						></IonInput>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Email</IonLabel>
+						<IonInput
+							required
+							clearInput
+							type="email"
+							placeholder="mrbean123@email.com"
+							name="email"
+							onIonChange={e =>
+								this.handleEmail((e.target as HTMLInputElement).value)
+							}
+						></IonInput>
+					</IonItem>
+					<IonItem>
+						<IonLabel>Password</IonLabel>
+						<IonInput
+							required
+							clearInput
+							onIonChange={e =>
+								this.handlePassword((e.target as HTMLInputElement).value)
+							}
+							type={
+								this.state.passwordVisibility === false
+									? PasswordVisibility.Password
+									: PasswordVisibility.Text
+							}
+							placeholder="123123123"
+							name="password"
+						></IonInput>
 
-            {this.state.password ? (
-              <IonIcon
-                icon={this.state.passwordVisibility ? eyeOff : eye}
-                onClick={this.togglePassword}
-              />
-            ) : (
-              <IonItem />
-            )}
-          </IonItem>
-          <IonButton onClick={this.createAccount}>Create Account</IonButton>
-          <IonButton onClick={this.props.showSignUp}>Back</IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  }
+						{this.state.password ? (
+							<IonIcon
+								icon={this.state.passwordVisibility ? eyeOff : eye}
+								onClick={this.togglePassword}
+							/>
+						) : (
+							<IonItem />
+						)}
+					</IonItem>
+					{this.props.signUpError ? (
+						<IonItem>password must be at least six characters!</IonItem>
+					) : null}
+					<IonButton onClick={this.createAccount}>Create Account</IonButton>
+					<IonButton onClick={this.props.showSignUp}>Back</IonButton>
+				</IonContent>
+			</IonPage>
+		)
+	}
 }
